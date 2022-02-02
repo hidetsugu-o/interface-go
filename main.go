a239a5d68371ec62c698a1fe63df1d1b0e6a94c1
hidetsugu-o <74892038+hidetsugu-o@users.noreply.github.com>
2022-02-03 08:20:07 +0900

    メイン側でインターフェース

--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
-func main() {
-	// NewではInterfaceが帰ってくる
-	client := New()
+import "fmt"
 
-	client.Get()
-	client.Post()
+type Dog struct{}
+
+func (Dog) Speak() { fmt.Println("bowwow") }
+func (Dog) Run()   { fmt.Println("yay, I'm running") }
+
+type Cat struct{}
+
+func (Cat) Speak() { fmt.Println("mewmew") }
+func (Cat) Run()   { fmt.Println("shut up") }
+
+type Runner interface {
+	Run()
+}
+
+func main() {
+	Race([]Runner{Dog{}, Cat{}})
+}
+
+func Race(runners []Runner) {
+	for _, runner := range runners {
+		runner.Run()
+	}
 }
