5301a4e3d59893c0f756ec62dd90d467896d8658
hidetsugu-o <74892038+hidetsugu-o@users.noreply.github.com>
2022-02-06 22:07:41 +0900

    robot追加

--- main.go
+++ main.go
@@ -12,12 +12,16 @@ type Cat struct{}
 func (Cat) Speak() { fmt.Println("mewmew") }
 func (Cat) Run()   { fmt.Println("shut up") }
 
+type Robot struct{}
+
+func (Robot) Run() { fmt.Println("geek geek") }
+
 type Runner interface {
 	Run()
 }
 
 func main() {
-	Race([]Runner{Dog{}, Cat{}})
+	Race([]Runner{Robot{}, Dog{}, Cat{}})
 }
 
 func Race(runners []Runner) {
